fb9fdff093e267c44403dd87ab8b5ba1e6afcd14
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-10-27 00:51:24 +0300

    add example005 random sum

---
+++ example005_randomSum/Program.cs
@@ -0,0 +1,6 @@
+﻿double numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+double result = numberA + numberB;
+Console.WriteLine(result);
