5e44552c31e71c0720d72599d80e6daff7376d50
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-10-27 01:08:23 +0300

    add example007 random max

---
+++ example007_max/Program.cs
@@ -0,0 +1,18 @@
+﻿double numberA = new Random().Next(1, 100);
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1, 100);
+Console.WriteLine(numberB);
+double numberC = new Random().Next(1, 100);
+Console.WriteLine(numberC);
+double numberD = new Random().Next(1, 100);
+Console.WriteLine(numberD);
+
+double max = numberA;
+
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+if (numberD > max) max = numberD;
+
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
