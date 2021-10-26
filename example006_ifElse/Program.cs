fac6eb790bcc6d4866a8116c43711ca94de368e8
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-10-27 01:00:09 +0300

    add example006 hello, love username

---
+++ example006_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "настя")
+{
+    Console.WriteLine("Ура, это же Настя!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
