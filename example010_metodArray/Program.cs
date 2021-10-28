ce8fabf7ba68d3213eae6a51504eaf87a5ed1917
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-10-28 19:51:51 +0300

    add example009 and example010

---
+++ example010_metodArray/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(int[] colection)
+{
+    int lenght = colection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        colection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
