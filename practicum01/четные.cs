2e4b5ac610a7174daa99876e0564add3ec0c3d10
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-11-01 23:53:24 +0300

    add practicum01

---
+++ practicum01/четные.cs
@@ -0,0 +1,36 @@
+﻿// Cформировать случайным образом целочисленный массив A из натуральных двузначных чисел. 
+//Создать на его основе масив B, отбрасывая те, которые
+//нарушают порядок возрастания
+//больше среднего арифметического элементов A
+//чётные
+
+int[] A = new int[8];
+int index = 0;
+
+while (index < 8)
+{
+    A[index] = new Random().Next(10, 100);
+    index++;
+}
+
+index = 0;
+while (index < 8)
+{
+    int array = A[index];
+    Console.WriteLine(array);
+    index++;
+}
+
+Console.WriteLine();
+
+index = 0;
+while (index <= 8)
+{
+    int B = A[index];
+    if (B % 2 == 0)
+    {
+        Console.WriteLine(B);
+        index++;
+    }
+    index++;
+}
